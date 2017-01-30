cdb3ba7ea9d1ce5691b0f8c7f6865b58e4be4c39
Jone Manneråk Skribeland <jone.skribeland@krs-vg-087.uia.no>
2017-01-30 08:51:29 +0100

    Endringer

---
+++ hello.go
@@ -0,0 +1,29 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset134 PingFangSC-Regular;}
+{\colortbl;\red255\green255\blue255;\red255\green255\blue207;\red0\green0\blue0;\red255\green255\blue255;
+\red135\green135\blue135;}
+{\*\expandedcolortbl;\csgray\c100000;\cssrgb\c100000\c100000\c84706;\cssrgb\c0\c0\c0;\csgray\c100000;
+\cssrgb\c60000\c60000\c60000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf0 \cb2 \expnd0\expndtw0\kerning0
+package main\cb4 \
+\pard\pardeftab720\partightenfactor0
+\cf5 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb2 import "fmt"\cb4 \
+\pard\pardeftab720\partightenfactor0
+\cf5 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb2 func main() \{\cb4 \
+\pard\pardeftab720\partightenfactor0
+\cf5 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb2     fmt.Println("Hello, 
+\f1 \'ca\'c0\'bd\'e7
+\f0 ")\cf5 \cb4 \
+\cf0 \cb2 \}\cf5 \cb4 \
+\cf0 \cb2 \'a0}
